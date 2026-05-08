0f807ce44b2622ca49fe48c73843c52cb8b58315
Ayşenur Navruz <aysenur.navruz@qkare.com>
2026-05-08 16:05:46 +0300

    add zod, tanstack query, react hook form, sonner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,19 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { TooltipProvider } from './components/ui/tooltip'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 document.documentElement.classList.add('dark')
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   
+    <QueryClientProvider  client={queryClient}>
     <TooltipProvider>
       <BrowserRouter>
         <App />
+        <Toaster position="top-right" richColors />
       </BrowserRouter>
     </TooltipProvider>
- 
+  </QueryClientProvider>
 )
